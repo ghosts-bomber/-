2fbb06babcdca8c0163a109e9a0f40d426469649
ghosts-bomber <36564109+ghosts-bomber@users.noreply.github.com>
2018-02-18 20:27:55 +0800

    Update backup_ver3.py

--- backup_ver3.py
+++ backup_ver3.py
@@ -16,7 +16,7 @@ now = time.strftime('%H%M%S')
 if len(comment) == 0:
     target = today + os.sep + now + '.zip'
 else:
-    target = today + os.sep + now + '_' +
+    target = today + os.sep + now + '_' + \
         comment.replace(' ','_') + '.zip'
 # 如果子目录不存在则创建一个
 if not os.path.exists(today):
